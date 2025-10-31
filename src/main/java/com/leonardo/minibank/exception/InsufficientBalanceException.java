2c2e4a0e24e7d94527c3d533ef1fc3cecb286291
Leonardo <leobarros_@outlook.com>
2025-10-31 20:18:44 -0300

    feat: add recordWithdraw method on TransactionService

---
+++ src/main/java/com/leonardo/minibank/exception/InsufficientBalanceException.java
@@ -0,0 +1,7 @@
+package com.leonardo.minibank.exception;
+
+public class InsufficientBalanceException extends RuntimeException {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/leonardo/minibank/service/TransactionService.java
+++ src/main/java/com/leonardo/minibank/service/TransactionService.java
@@ -1,6 +1,7 @@
 package com.leonardo.minibank.service;
 
 import com.leonardo.minibank.dto.transaction.TransactionDTO;
+import com.leonardo.minibank.exception.InsufficientBalanceException;
 import com.leonardo.minibank.exception.InvalidTransactionAmountException;
 import com.leonardo.minibank.model.Account;
 import com.leonardo.minibank.model.Transaction;
@@ -23,5 +24,14 @@ public class TransactionService {
         Transaction transaction = new Transaction(null, TransactionType.DEPOSIT, amount, account);
         transactionRepository.save(transaction);
         return new TransactionDTO(transaction.getId(),transaction.getType(), transaction.getValue(), LocalDateTime.now(), account.getId());
+    }
+
+    public TransactionDTO recordWithdraw(Account account, Double amount){
+        if(amount <= 0) throw new InvalidTransactionAmountException("Amount must be greater than zero");
+        if(account.getBalance() < amount) throw new InsufficientBalanceException("Insufficient balance: attempted " + amount + ", available " + account.getBalance());
+        account.setBalance(account.getBalance() - amount);
+        Transaction transaction = new Transaction(null, TransactionType.WITHDRAW, amount, account);
+        transactionRepository.save(transaction);
+        return new TransactionDTO(transaction.getId(), transaction.getType(), transaction.getValue(), LocalDateTime.now(), account.getId());
     }
 }
